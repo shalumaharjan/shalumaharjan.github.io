babb1ff76610135f50f18805725c803f69af7bd4
Shalu Maharjan <shalumaharjan15@gmail.com>
2026-02-14 21:23:31 +0545

    Update: added back to top functionality

---
+++ js/back-to-top.js
@@ -0,0 +1,14 @@
+let mybutton = document.getElementById("back_to_top");
+
+// When the user scrolls down 30px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
